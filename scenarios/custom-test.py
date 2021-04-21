9d9792592a02bef64f81b37e05a7a112bbca978a
admin <andrew.marriage@dataiku.com>
2021-04-21 18:04:19 -0400

    Saved scenario BUNDLESS.custom-test (id: custom-test)

--- scenarios/custom-test.py
+++ scenarios/custom-test.py
@@ -8,10 +8,12 @@ sender = scenario.get_message_sender(channel_id =  "gmail") # A messaging channe
 
 # Define your attachment
 attachment = {"destinationType":"DOWNLOAD","destinationDatasetProjectKey":"DKU_CHURN","overwriteDestinationDataset":"false","selection":{"samplingMethod":"FULL","partitionSelectionMethod":"ALL","targetRatio":0.02,"maxRecords":100000,"selectedPartitions":[],"ordering":{"enabled":"false","rules":[]}},"advancedMode":"false","$exportOption":{"id":"excel","label":"Excel (*.xlsx)","canStream":"false","formatType":"excel","predefinedConfig":{"xlsx":"true","preserveNumberFormatting":"false","parseDatesToISO":"false","skipRowsBeforeHeader":0,"parseHeaderRow":"false","skipRowsAfterHeader":0},"optionType":"BUILTIN_FORMAT","$$hashKey":"object:31922"},"originatingOptionId":"excel","format":{"type":"excel","params":{"xlsx":"true","preserveNumberFormatting":"false","parseDatesToISO":"false","skipRowsBeforeHeader":0,"parseHeaderRow":"false","skipRowsAfterHeader":0}}}
+
+print(attachment)
 # Add the attachment to the sender
 sender.set_params(sender="andrew.marriage@dataiku.com",
              recipient="andrew.marriage@dataiku.com", #multiple emails 
-             attachments = attachment,
+             attachments = [attachment],
              subject="Hello Jed",
              message="Test to test the testy test.")
 # fire away
