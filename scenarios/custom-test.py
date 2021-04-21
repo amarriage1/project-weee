0c49669d687544c20bf3db1db96e223e65a913b3
admin <andrew.marriage@dataiku.com>
2021-04-21 17:41:49 -0400

    Saved scenario BUNDLESS.custom-test (id: custom-test)

--- scenarios/custom-test.py
+++ scenarios/custom-test.py
@@ -1,24 +1,54 @@
-# This sample code helps you get started with the custom scenario API.
-#For more details and samples, please see our Documentation
+# Get senario handler
 from dataiku.scenario import Scenario
-
-# The Scenario object is the main handle from which you initiate steps
+​
 scenario = Scenario()
-
-# A few example steps follow
-
-# Building a dataset
-scenario.build_dataset("customers_prepared", partitions="2015-01-03")
-
-# Controlling the train of a dataset
-train_ret = scenario.train_model("uSEkldfsm")
-trained_model = train_ret.get_trained_model()
-performance = trained_model.get_new_version_metrics().get_performance_values()
-if performance["AUC"] > 0.85:
-    trained_model.activate_new_version()
-
-# Sending custom reports
-sender = scenario.get_message_sender("mail-scenario", "local-mail") # A messaging channel
-sender.set_params(sender="dss@company.com", recipient="data-scientists@company.com")
-
-sender.send(subject="The scenario is doing well", message="All is good")
+​
+# Create a message sender
+sender = scenario.get_message_sender(channel_id =  "jeddy") # A messaging channel
+​
+# Define your attachment
+attachment =     {
+                  "type": "DATASET",
+                  "params": {
+                    "exportParams": {
+                      "destinationType": "DOWNLOAD",
+                      "destinationDatasetProjectKey": "DISCRETEPARTITIONING", # Set your project key here
+                      "overwriteDestinationDataset": False,
+                      "selection": {
+                        "samplingMethod": "FULL",
+                        "partitionSelectionMethod": "ALL",
+                        "targetRatio": 0.02,
+                        "maxRecords": 100000,
+                        "selectedPartitions": [],
+                        "ordering": {
+                          "enabled": False,
+                          "rules": []
+                        }
+                      },
+                      "advancedMode": False,
+                      "originatingOptionId": "excel",
+                      "format": {
+                        "type": "excel",
+                        "params": {
+                          "xlsx": True,
+                          "preserveNumberFormatting": False,
+                          "parseDatesToISO": False,
+                          "skipRowsBeforeHeader": 0,
+                          "parseHeaderRow": True,
+                          "skipRowsAfterHeader": 0
+                        }
+                      }
+                    },
+                    "attachedDataset": "customers_discrete", # Set your dataset here
+                    "partitionId": "s3" # Set your partition here
+                  }
+                }
+​
+# Add the attachment to the sender
+sender.set_params(sender="jed.dougherty@dataiku.com",
+             recipient="jed.dougherty@dataiku.com", #multiple emails 
+             attachments = [attachment],
+             subject="Hello Jed",
+             message="Test to test the testy test.")
+# fire away
+sender.send()
